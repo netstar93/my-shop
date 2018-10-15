7cc42105bbdcdb8c5beda3cfd4976214ddb8d88e
Nick <nickdev@gmail.com>
2018-10-15 19:16:56 +0530

    15 oct

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,7 @@
 @section('title', 'Login')
 @extends('admin.layout')
 @section('content')
-<div class="right-side" >
+<div class="right-side rounded" >
 	<div class="page-content">
 		<section class="page-header">
 			<h3>Welcome to Dashboard </h3>
